c0f257d9e944c3b61bf5507adfaf84ed675263f8
ByF3lix <haife@DESKTOP-BK0SDEG>
2023-07-25 18:32:16 +0200

    Order Datenbank; Orders anzeigen Website

---
+++ MensaWebsite/Controllers/OrderController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MensaWebsite.Controllers
+{
+    public class OrderController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ShowAllOrders()
+        {
+            return View();
+        }
+    }
+}
